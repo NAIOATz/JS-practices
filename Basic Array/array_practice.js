1e422724899fd79a23de53410b96c8b7859330d7
NAIOATz <surapoj6029@gmail.com>
2025-08-25 04:21:31 +0700

    modify file array-practice

--- Basic Array/array_practice.js
+++ Basic Array/array_practice.js
@@ -1,44 +1,44 @@
 // 1. สร้าง array ที่เก็บชื่อเพื่อน 5 คน แล้วพิมพ์ชื่อแต่ละคนออกมา
 
-// friends = ["Kiki", "Hero", "V", "Bee", "A"]
+friends = ["Kiki", "Hero", "V", "Bee", "A"]
 
-// for (const friend of friends){
-//     console.log(friend)
-// }
+for (const friend of friends){
+    console.log(friend)
+}
 
 // 2. สร้าง array ของตัวเลข 1–10 แล้วพิมพ์ทุกตัว
 
-// number = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10]
-// for (const num of number){
-//     console.log(num)
-// }
+number = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10]
+for (const num of number){
+    console.log(num)
+}
 
 // 3. เขียนฟังก์ชันที่รับ array ของตัวเลข แล้วคืนค่า array ที่มีเลขทุกตัวคูณ 2
 
-// function multiple(arr){
-//     const output = []
-//     for (const num of arr){
-//         result = num * 2
-//         output.push(result)
+function multiple(arr){
+    const output = []
+    for (const num of arr){
+        result = num * 2
+        output.push(result)
         
-//     }
-//     return output
-// }
+    }
+    return output
+}
 
-// console.log(multiple([1,2,3,4,5]))
+console.log(multiple([1,2,3,4,5]))
 
 // 4. เขียนฟังก์ชันที่รับ string array แล้วคืนค่า array ของความยาวตัวอักษรแต่ละคำ
 
-// function stringArr(arr){
-//     const output = []
-//     for (word of arr){
-//         result = word.length
-//         output.push(result)
-//     }
-//     return output
-// }
+function stringArr(arr){
+    const output = []
+    for (word of arr){
+        result = word.length
+        output.push(result)
+    }
+    return output
+}
 
-// console.log(stringArr(["Banana", "Apple", "Kiwi", "A"]))
+console.log(stringArr(["Banana", "Apple", "Kiwi", "A"]))
 
 // 5. ตรวจสอบว่า array มีค่า “JavaScript” หรือไม่ หากมีให้พิมพ์ “พบแล้ว”
 
